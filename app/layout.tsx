7b038e56b7961b83b402b647002f44b3b92c7f4d
Jen Breese-Kauth <jbreese@stanford.edu>
2025-09-09 16:37:05 -0700

    Added tailwind and started header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,43 @@ export const metadata: Metadata = {
   description: 'Dashboard for visualizing Acquia API data',
 };
 
+function StanfordHeader() {
+  return (
+    <header className="stanford-header">
+      <div className="su-brand-bar su-brand-bar--bright">
+        <div className="su-brand-bar__container">
+          <a className="su-brand-bar__logo" href="https://stanford.edu">Stanford University</a>
+        </div>
+      </div>
+      <div className="stanford-header-bar">
+        <div className="su-lockup__wordmark-wrapper">
+          <span className="su-lockup__wordmark text-mint-500">Stanford</span>
+        </div>
+        <div className="stanford-header-title">
+          <span>Cloud Hosting Usage Reporting with Recurring Output (CHURRO)</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function StanfordFooter() {
+  return (
+    <footer className="stanford-footer">
+      <div className="stanford-footer-bar">
+        <div>
+          <a href="https://www.stanford.edu/" target="_blank" rel="noopener noreferrer" className="stanford-footer-link">
+            Stanford University
+          </a>
+        </div>
+        <div className="stanford-footer-copy">
+          &copy; {new Date().getFullYear()} Stanford University | Stanford Web Services
+        </div>
+      </div>
+    </footer>
+  );
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -16,7 +53,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StanfordHeader />
+        <main>{children}</main>
+        <StanfordFooter />
+        </body>
     </html>
   );
 }
