07949086fa6b940e26c13d8d6f8e7d05b40ed1c4
Jen Breese-Kauth <jbreese@stanford.edu>
2025-10-22 17:47:07 -0700

    Reverted to the older version on bar charts

--- components/SimpleVisitsBarChart.tsx
+++ components/SimpleVisitsBarChart.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { BarChart } from '@mui/x-charts/BarChart';
-import { Box } from '@mui/material';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip } from 'recharts';
 
 interface SimpleVisitsBarChartProps {
   data: { name: string; value: number }[];
@@ -13,52 +12,23 @@ const SimpleVisitsBarChart: React.FC<SimpleVisitsBarChartProps> = ({ data }) => 
     name: item.name,
     visits: item.value
   }));
-  
+
   return (
-    <Box 
-      sx={{ 
-        width: '100%', 
-        height: 384, // h-96 = 384px
-        bgcolor: 'white', 
-        p: 2, 
-        borderRadius: 2 
-      }}
-    >
-      <BarChart
-        dataset={chartData}
-        yAxis={[{ 
-          scaleType: 'band', 
-          dataKey: 'name',
-          categoryGapRatio: 0.3,
-          barGapRatio: 0.1,
-        }]}
-        xAxis={[{ 
-          label: 'Visits',
-        }]}
-        series={[
-          { 
-            dataKey: 'visits', 
-            label: 'Visits',
-            color: '#8C1515', // Cardinal red
-            valueFormatter: (value) => value?.toLocaleString() ?? '0',
-          }
-        ]}
-        layout="horizontal"
-        grid={{ vertical: true, horizontal: true }}
-        margin={{ top: 10, right: 30, left: 150, bottom: 40 }}
-         slotProps={{
-          legend: { sx: { display: 'none' } },
-        }}
-        sx={{
-          '& .MuiChartsAxis-tickLabel': {
-            fontSize: '13px',
-          },
-          '& .MuiChartsAxis-label': {
-            fontSize: '16px',
-          },
-        }}
-      />
-    </Box>
+    <div className="w-full h-96 bg-white p-4 rounded-lg">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart 
+          data={chartData} 
+          layout="vertical" 
+          margin={{ top: 5, right: 30, left: 150, bottom: 5 }}
+          barGap={8}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="number" tick={{ fontSize: 16 }} />
+          <YAxis dataKey="name" type="category" width={300} interval={0} tick={{ fontSize: 13 }} />
+          <Tooltip formatter={(value: number) => [value.toLocaleString(), 'Visits']} cursor={{fill: 'rgba(206, 206, 206, 0.2)'}} />
+          <Bar dataKey="visits" className='fill-cardinal-red' />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
